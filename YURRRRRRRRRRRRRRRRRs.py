8cb1cabf67bf97837b60105450a02ae53ac8ba4d
TheMakr <typicalmichael23@gmail.com>
2024-09-06 12:55:55 -0400

    Add files via upload

---
+++ YURRRRRRRRRRRRRRRRRs.py
@@ -0,0 +1,38 @@
+print("You come to a split in the road. Do you go right or left?")
+choice=input("Type 1 to go left, Type 2 to go right:")
+if choice=="1":
+    print (" You ran into Requiese Jeqavious The 5th and he gifted you the Overwatch battle pass.")
+
+    choice2=input("There is a cast iron skillet. Type 1 to smack Requiese with it because Overwatch sucks. Type 2 to accept the battle pass:")
+    if choice2=="1":
+        print("Requiese died.")
+        choice6=input ("The cops come after you. Type 1 to run away, Type 2 to give in to the cops:")
+        if choice6=="1":
+            print("The cops were'nt playing and they shot you on site.")
+        else:
+            print("You went to prison and met Requiese's brother")
+    else:
+        print("You became a crippling Overwatch addict and forgot what the outside world and shower was.")
+    choice7=input ("You have to play comp or quickplay. Type 1 to play comp, Type 2 to play quickplay:")
+    if choice7=="1":
+            print("You entered the deepest pit of pain and agony and regret you're decision of claiming the battlepass.")
+    else:
+            print("You play quickplay and have somewhat of a decent time but in the end nobody likes Overwatch so you still wish you never took the battlepass.")
+else:
+    print ("You walked into a nuclear power plant and died to radiation.")
+
+    choice3=input("There is a stairway and a highway. Type 1 to go down the highway, Type 2 to go up the stairway:")
+    if choice3=="1":
+        print ("You ended up in Hell and the Devil burns you eternally because you were a Maugua main in Overwatch.")
+        choice4=input("The Devil offers you a deal. Type 1 to take the deal, Type 2 to decline the offer:")
+        if choice4=="1":
+            print("You challenge the Devil to an intense Dance Dance Revolution Battle.")
+        else:
+            print("The Devil forces you to watch Skibidi Toilet while burning eternally.")
+    else:
+         print ("You met Jesus and he held you in his arms becaue you followed your dreams of dying like a fallout character.")
+         choice5=input("Jesus wants to turn you into an angel to do something for him. Type 1 to accept the offer, Type 2 to decline the offer:")
+         if choice5=="1":
+             print("You do as Jesus says and go on a jounry back to the real world to rain down liberatation on all Overwatch players.")
+         else:
+             print("You declined the offer and Jesus was disappointed, but didn't really care, and you have a peaceful afterlife in heaven.")
